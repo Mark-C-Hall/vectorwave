93f4ee67d8a4aab2be5cd18f8fe9cb526e3642ff
Mark Hall <mark.hall993@gmail.com>
2024-03-05 15:09:21 -0700

    Remove Users Model

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { userRouter } from "~/server/api/routers/user";
-import { conversationRouter } from "./routers/conversation";
+import { conversationRouter } from "~/server/api/routers/conversation";
 import { messageRouter } from "~/server/api/routers/message";
 
 /**
@@ -9,7 +8,6 @@ import { messageRouter } from "~/server/api/routers/message";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  user: userRouter,
   conversation: conversationRouter,
   message: messageRouter,
 });
